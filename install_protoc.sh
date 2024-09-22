b73e9583a0ca73520a2650d43365f407110b2ba6
Walter Schulze <awalterschulze@gmail.com>
2024-09-22 13:10:13 +0100

    simplify install_protoc.sh

--- install_protoc.sh
+++ install_protoc.sh
@@ -2,27 +2,8 @@
 
 set -ex
 
-die() {
-    echo "$@" >&2
-    exit 1
-}
-
 cd ${HOME}
 
-case "$PROTOBUF_VERSION" in
-2*)
-    basename=protobuf-$PROTOBUF_VERSION
-    wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz
-    tar xzf $basename.tar.gz
-    cd protobuf-$PROTOBUF_VERSION
-    ./configure --prefix=${HOME} && make -j2 && make install
-    ;;
-3*)
-    basename=protoc-$PROTOBUF_VERSION-linux-x86_64
-    wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.zip
-    unzip $basename.zip
-    ;;
-*)
-    die "unknown protobuf version: $PROTOBUF_VERSION"
-    ;;
-esac
+basename=protoc-3.14.0-linux-x86_64
+wget https://github.com/google/protobuf/releases/download/v3.14.0/$basename.zip
+unzip $basename.zip
