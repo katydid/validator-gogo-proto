acbd8d4a2bf9dc1d87e7c81e67f2bfe8668c5a3a
Walter Schulze <awalterschulze@gmail.com>
2025-01-16 17:19:11 +0000

    update protoc version

--- install_protoc.sh
+++ install_protoc.sh
@@ -4,6 +4,6 @@ set -ex
 
 cd ${HOME}
 
-basename=protoc-3.14.0-linux-x86_64
-wget https://github.com/google/protobuf/releases/download/v3.14.0/$basename.zip
+basename=protoc-29.3-linux-x86_64
+wget https://github.com/protocolbuffers/protobuf/releases/download/v29.3/$basename.zip
 unzip $basename.zip
